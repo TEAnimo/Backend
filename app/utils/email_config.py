ea516afb15e23a46a4758de43279fb21d0a05077
ErnestoSCL <ernestosaniel123@gmail.com>
2025-06-12 22:20:54 -0500

    Dominio de correo actualizado

--- app/utils/email_config.py
+++ app/utils/email_config.py
@@ -8,9 +8,10 @@ conf = ConnectionConfig(
     MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
     MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
     MAIL_FROM=os.getenv("MAIL_USERNAME"),
+    MAIL_FROM_NAME=os.getenv("MAIL_FROM_NAME"),
     MAIL_PORT=587,
-    MAIL_SERVER="smtp.gmail.com",
-    MAIL_STARTTLS=True,
+    MAIL_SERVER="smtp.zoho.com",
     MAIL_SSL_TLS=False,
+    MAIL_STARTTLS=True,
     USE_CREDENTIALS=True
 )
